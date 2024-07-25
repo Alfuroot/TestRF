ca554ccf106bd18f3f78ec861d169b467747e071
Alfuroot <59538409+Alfuroot@users.noreply.github.com>
2024-07-26 01:15:34 +0200

    Initial Commit

---
+++ TestRF/TestRFApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestRFApp.swift
+//  TestRF
+//
+//  Created by Giuseppe Carannante on 26/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct TestRFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
